e55327ece904718e253d997905025b77abe7fca9
Farago Balazs <minimalgeek@gmail.com>
2018-07-02 14:53:23 +0200

    Sample test

--- src/main/java/com/example/sessiondemo/SessiondemoApplication.java
+++ src/main/java/com/example/sessiondemo/SessiondemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Range;
 
 import java.util.List;
 import java.util.stream.Collectors;
---
+++ src/test/java/com/example/sessiondemo/service/SessionServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.sessiondemo.service;
+
+import com.example.sessiondemo.SessiondemoApplicationTests;
+import com.example.sessiondemo.model.Car;
+import com.example.sessiondemo.repository.CarRepository;
+import org.assertj.core.util.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class SessionServiceTest extends SessiondemoApplicationTests {
+
+    @MockBean
+    private CarRepository repository;
+
+    @Autowired
+    @InjectMocks
+    private SessionService sessionService;
+
+    @Before
+    public void setUp() throws Exception {
+        Car car = Car.randomCar();
+        car.setId(0L);
+        when(repository.findAll()).thenReturn(Lists.newArrayList(car));
+        when(repository.findById(0L)).thenReturn(Optional.of(car));
+    }
+
+    @Test
+    public void buyCar() {
+        sessionService.buyCar(0L);
+        assertEquals(1, sessionService.getCarsInShoppingCart().size());
+    }
+
+    @Test
+    public void getCarsInShoppingCart() {
+    }
+}
